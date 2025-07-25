a9514b965967c07de38361c419e536e06eec0b18
felix <felix.klaedtke@gmail.com>
2025-07-25 19:05:22 +0200

    effect translation

--- funcs.go
+++ funcs.go
@@ -9,7 +9,7 @@ import (
 
 
 // `effect` returns the escape sequence as a string for setting the specified
-// effects `effs`. 
+// effects `effs`.
 func effect(effs ...int) string {
     if len(effs) == 0 {
         return reset
@@ -39,7 +39,7 @@ func effect(effs ...int) string {
 var NoColor bool
 
 
-// `Effect` returns a string for the effects `effs`.  The string consists of the 
+// `Effect` returns a string for the effects `effs`.  The string consists of the
 // respective terminal color escape sequences.  If `effs` is empty, all effects
 // are turned off.  The returned string can, e.g., be used in fmt.Printf calls.
 func Effect(effs ...int) string {
@@ -72,18 +72,52 @@ func FSetEffect(w io.Writer, effs ...int) error {
 
 type ColorCode struct {
     // If true, ignore color codes.
-    NoColor bool
+    NoColor   bool
+    // Translation color codes.
+    Translate map[int][]int
+    // If true, keep effects that are not in the translation map.
+    Keep      bool
+}
+
+func (c ColorCode) translate(effs []int) []int {
+    if c.Translate == nil {
+        // No translation of effects.
+        return effs
+    }
+
+    var rs []int
+    for _, eff := range effs {
+        if vs, ok := c.Translate[eff]; ok {
+            rs = append(rs, vs...)
+        } else if c.Keep {
+            // If c.Keep is false, all effects that are not in the translation
+            // map are filtered out.  If c.Keep is true, one must map eff to the
+            // empty slice to filter the effect out.
+            rs = append(rs, eff)
+        }
+    }
+    return rs
 }
 
 func (c ColorCode) Effect(effs ...int) string {
     if c.NoColor {
         return ""
     }
-    return effect(effs...)
+    teffs := c.translate(effs)
+    if len(teffs) == 0 && len(effs) != 0 {
+        // All effects are filtered out.  But since effs is not empty, do not
+        // reset.
+        return ""
+    }
+    return effect(teffs...)
 }
 
 func (c ColorCode) SetEffect(effs ...int) error {
     if c.NoColor {
+        return nil
+    }
+    teffs := c.translate(effs)
+    if len(teffs) == 0 && len(effs) != 0 {
         return nil
     }
     _, err := fmt.Printf("%s", effect(effs...))
@@ -93,7 +127,11 @@ func (c ColorCode) SetEffect(effs ...int) error {
 func (c ColorCode) FSetEffect(w io.Writer, effs ...int) error {
     if c.NoColor {
         return nil
-    }  
+    }
+    teffs := c.translate(effs)
+    if len(teffs) == 0 && len(effs) != 0 {
+        return nil
+    }
     _, err := fmt.Fprintf(w, "%s", effect(effs...))
     return err
 }
--- termcol_test.go
+++ termcol_test.go
@@ -6,7 +6,7 @@ import (
 )
 
 
-// The following tests do not fail.  Run them as `go test -v` and check 
+// The following tests do not fail.  Run them as `go test -v` and check
 // the (colorful and blinking) output.
 
 func TestForeground(t *testing.T) {
@@ -35,6 +35,9 @@ func TestCombinedStyle(t *testing.T) {
 
 
 func ExampleEffect() {
+    // Delete or set to false to see colorized output.
+    NoColor = true
+
     // Output text in various foreground colors
     fmt.Printf("Foreground: %sBlack%sRed%sGreen%sYellow%sBlue%sMagenta%sCyan%sWhite%s\n",
         Effect(FgBlack), Effect(FgRed), Effect(FgGreen), Effect(FgYellow), Effect(FgBlue), Effect(FgMagenta), Effect(FgCyan), Effect(FgWhite),
@@ -47,11 +50,21 @@ func ExampleEffect() {
 
     // Output text in red, bold, and underlined
     fmt.Printf("%sText %sText also underlined%s Text%s\n",
-        Effect(FgRed, Bold), Effect(Underline), Effect(UnderlineOff), 
+        Effect(FgRed, Bold), Effect(Underline), Effect(UnderlineOff),
         Effect())
+
+    NoColor = false
+
+    // Output:
+    // Foreground: BlackRedGreenYellowBlueMagentaCyanWhite
+    // Background: BlackRedGreenYellowBlueMagentaCyanWhite
+    // Text Text also underlined Text
 }
 
 func ExampleSetEffect() {
+    // Delete or set to false to see colorized output.
+    NoColor = true
+
     SetEffect()
     fmt.Printf("default")
 
@@ -62,7 +75,7 @@ func ExampleSetEffect() {
     // Output some text witn reversed colors, i.e., blue (foreground) and red (background)
     SetEffect(Reverse)
     fmt.Printf("reversed")
-    
+
     // Back to red (foreground) and blue (background) plus underlined
     SetEffect(ReverseOff, Underline)
     fmt.Printf("underlined and colored")
@@ -70,4 +83,37 @@ func ExampleSetEffect() {
     // Turn off all effects
     SetEffect()
     fmt.Printf("default again\n")
+
+    NoColor = false
+
+    // Output:
+    // defaultcoloredreversedunderlined and coloreddefault again
+}
+
+
+func ExampleColorCode() {
+    const (
+        Info = 1
+        Warn = 2
+        Err  = 3
+    )
+    coloring := ColorCode{
+        NoColor:  true, // Delete or set to false to see colorized output.
+        Translate: map[int][]int{
+            Info: []int{BgYellow, Bold},
+            Warn: []int{BgMagenta, Bold},
+            Err:  []int{BgRed, Bold},
+        },
+    }
+
+    fmt.Printf("%s[I]%s Some information.\n", coloring.Effect(Info), coloring.Effect())
+    fmt.Printf("%s[W]%s A warning.\n", coloring.Effect(Warn), coloring.Effect())
+    fmt.Printf("%s[E]%s An error message.\n", coloring.Effect(Err), coloring.Effect())
+    fmt.Printf("%sno color effect, since filtered out\n", coloring.Effect(Underline, Blink, FgGreen))
+
+    // Output:
+    // [I] Some information.
+    // [W] A warning.
+    // [E] An error message.
+    // no color effect, since filtered out
 }
